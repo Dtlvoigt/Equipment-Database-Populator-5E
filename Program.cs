499ac70a34dc577c7a7c9cb930918aaa6ca47239
Dylan Voigt <dtlvoigt@gmail.com>
2024-10-02 17:09:34 -0700

    deleted commented out code

--- Program.cs
+++ Program.cs
@@ -12,13 +12,11 @@ namespace EquipmentDatabasePopulator5E
                .SetBasePath(Directory.GetCurrentDirectory())
                .AddJsonFile("appsettings.json")
                .Build();
-            //.Build();
 
             //retrieve connection string
-            //IConfigurationRoot configuration = configurationBuilder.Build();
             string connectionString = configurationBuilder.GetConnectionString("DefaultConnection");
 
-            // Set up DbContext options
+            //set up DbContext options
             var optionsBuilder = new DbContextOptionsBuilder<EquipmentContext>()
                 .UseSqlServer(connectionString);
 
@@ -42,27 +40,6 @@ namespace EquipmentDatabasePopulator5E
                 await LoadService(context);
             }
         }
-
-        //public ApplicationDbContext CreateDbContext(string[] args)
-        //{
-        //    // Set up the configuration to read from appsettings.json
-        //    var configurationBuilder = new ConfigurationBuilder()
-        //        .SetBasePath(Directory.GetCurrentDirectory())
-        //        .AddJsonFile("appsettings.json")
-        //        .Build();
-        //    //.Build();
-
-        //    // Retrieve the connection string
-        //    //IConfigurationRoot configuration = configurationBuilder.Build();
-        //    string connectionString = configurationBuilder.GetConnectionString("DefaultConnection");
-
-
-        //    // Set up DbContext options
-        //    var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>()
-        //        .UseSqlServer(connectionString);
-
-        //    return new ApplicationDbContext(optionsBuilder.Options);
-        //}
 
         static async Task LoadService(EquipmentContext context)
         {
